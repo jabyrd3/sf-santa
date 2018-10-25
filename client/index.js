04f2c976051edd294b9d3debc9f9e155aba872d9
Jordan Byrd <jabyrd3@gmail.com>
2018-10-24 21:06:44 -0400

    apt

--- client/index.js
+++ client/index.js
@@ -28,7 +28,7 @@ module.exports = () => `<!doctype html>
               </div>
               <div>
                 <label for="address-2">apt</label>
-                <input id="address-line2" name="address-line2" required autocomplete="address-line2"></input>
+                <input id="address-line2" name="address-line2" autocomplete="address-line2"></input>
               </div>
             </div>
             <div class="flex-wrap">
