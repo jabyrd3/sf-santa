1250d8edc0f58d41a94ae0ff53cbb026023a4364
Jordan Byrd <jabyrd3@gmail.com>
2018-11-02 00:33:57 -0400

    cutoff

--- client/index.js
+++ client/index.js
@@ -15,52 +15,7 @@ module.exports = () => `<!doctype html>
     <div class="wreath"></div>
       <div class="container">
         <h1>first annual street fighters<br/><span class="orange">SECRET GRITTY</span> gift x-change</h1>
-        <form method="POST" action="/submit" autocomplete="on">
-            <label for="name">your name</label>
-            <input id="fullname" type="text" name="name" autocomplete="name" required/>
-
-            <label for="email">your email (just for x-change updates)</label> 
-            <input id="email" type="email" name="email" required autocomplete="email"/>
-            <div class="flex-wrap">
-              <div>
-                <label for="street-address">street</label>
-                <input id="address-line1" name="street-address" required autocomplete="address-line1"></input>
-              </div>
-              <div>
-                <label for="address-2">apt</label>
-                <input id="address-line2" name="address-line2" autocomplete="address-line2"></input>
-              </div>
-            </div>
-            <div class="flex-wrap">
-              <div>
-                <label for="city">city</label>
-                <input id="city" name="city" required autocomplete="address-level2"></input>
-              </div>
-              <div>
-                <label for="state">state</label>
-                <input id="state" name="state" required autocomplete="address-level1"></input>
-              </div>
-              <div>
-                <label for="zip">zip</label>
-                <input id="zip" name="zip" required autocomplete="postal-code"></input>
-              </div>
-            </div>
-            <div class="flex-wrap">
-              <div>
-                <label for="country">Country</label>
-                <input type="text" maxlength="3" name="country">
-              </div>
-              <div>
-                <label for="international">i would be cool with shipping internationally</label>
-              </div>
-              <div>
-                <input type="checkbox" name="international"/>
-              </div>
-            </div>
-            <div class="flex-wrap center">
-              <button role="submit">send</button>
-            </div>
-        </form>
+        <h2>Sorry, open registration for 2018 is now closed. contact the mods if you missed the cutoff and we can try to work something out.</h2>
         <div class="gritty"></div>
         <div class="snow-container">
           <div class="snow foreground"></div>
