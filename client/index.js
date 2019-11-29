680fb8f9c8e1c063cc5dc85fde80b0e0086441d3
Jordan Byrd <jabyrd3@gmail.com>
2019-11-29 14:23:49 -0500

    twea

--- client/index.js
+++ client/index.js
@@ -16,11 +16,11 @@ module.exports = () => `<!doctype html>
       <div class="container">
         <h1>second annual street fighters secret santa gift x-change</h1>
         <form method="POST" action="/submit" autocomplete="on">
-            <label for="name">your name</label>
-            <input id="fullname" type="text" name="name" autocomplete="name" required/>
-
             <label for="name">the name you use on facebook</label>
             <input id="fbname" type="text" name="fbname" autocomplete="fbname" required/>
+
+            <label for="name">your name</label>
+            <input id="fullname" type="text" name="name" autocomplete="name" required/>
 
             <label for="email">your email (just for x-change updates)</label> 
             <input id="email" type="email" name="email" required autocomplete="email"/>
