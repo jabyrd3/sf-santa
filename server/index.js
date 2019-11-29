599133b1d9cff6306fac8e64b8fcb33a658ce969
Jordan Byrd <jabyrd3@gmail.com>
2019-11-29 14:00:36 -0500

    gdi

--- server/index.js
+++ server/index.js
@@ -118,7 +118,7 @@ app.post('/submit', (req, res)=>{
       client.query({
         name: 'testingupdate',
         text: 'INSERT INTO santa (name, fbname, email, address, recipient, uuid, address2, city, state, zip, international, country) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *',
-        values: [req.body.name, req.body.fbname, req.body.email, req.body['street-address'], null, uuidv4(), req.body['address-line-2'], req.body.city, req.body.state, req.body.zip, req.body.international||false, req.body.country || null],
+        values: [req.body.name, req.body.fbname, req.body.email, req.body['street-address'], null, uuidv4(), req.body['address-line2'], req.body.city, req.body.state, req.body.zip, req.body.international||false, req.body.country || null],
       })
       .then(result => {
         console.log(`${req.body.name} signed up for secret santa, id is ${result.rows[0].id}`);
